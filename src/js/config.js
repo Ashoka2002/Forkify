a01036db91714efa588107ec8c7f71e0f7aff874
Ashok Choudhary <ashoka8769@gmail.com>
2024-06-26 12:18:23 +0530

    update

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RESULT_PER_PAGE = 10;
-export const API_KEY = '151b183c-ab23-4710-b7fe-072baf04971d';
+export const API_KEY = 'use your own';
 export const CLOSE_FORM_TIME = 2500;
